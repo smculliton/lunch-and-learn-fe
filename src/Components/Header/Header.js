8a562eb6ad6e167ed2672b1f8a7e1c9f7d88d88f
Sean Culliton <smculliton@gmail.com>
2023-03-07 15:35:34 -0700

    Add nav links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return(
     <>
       <h1>Lunch and Learn</h1>
-      <h2>Cookin and Bookin</h2>
+      <Link to='/'>Home</Link> | <Link to='/register'>Register</Link> | <Link to='/login'>Login</Link>
+      <br/>
     </>
   )
 }
