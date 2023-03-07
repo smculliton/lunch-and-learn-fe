5bbc582c9d54c5b9636477199b7350e694a17288
Sean Culliton <smculliton@gmail.com>
2023-03-07 15:25:00 -0700

    fix search page

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -2,22 +2,26 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import RecipeCard from '../RecipeCard/RecipeCard'
-import Header from '../Header/Header'
 
 function SearchPage() {
   const [results, setResults] = useState([])
   const { keyword } = useParams()
-  console.log(keyword)
 
-  const handleSubmit = () => {
+  useEffect(() => {
     fetch(`http://localhost:4000/api/v1/recipes?country=${keyword}`)
       .then(response => response.json())
       .then(data => {
         setResults(
-          data.data.map(d => <RecipeCard info={d.attributes} />)
+          data.data.map((ele, i) => <RecipeCard info={ele.attributes} key={i}/>)
         )
       })
-  }
+  }, [])
+
+  return (
+    <>
+      {results}
+    </>
+  )
 }
 
 export default SearchPage
