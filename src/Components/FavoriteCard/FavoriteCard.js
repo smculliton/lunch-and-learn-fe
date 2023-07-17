089edc250ef7f8e7905055b40c697fe420cb63d2
Sean Culliton <smculliton@gmail.com>
2023-07-17 13:09:40 -0600

    Resolve bug that favorite images go stale after an amount of time

--- src/Components/FavoriteCard/FavoriteCard.js
+++ src/Components/FavoriteCard/FavoriteCard.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useUpdateUser } from '../../Providers/UserContext'
 import { Link } from 'react-router-dom'
 import { MdDeleteOutline } from 'react-icons/md'
+import logo from '../../images/logo2.png'
 
 import './_FavoriteCard.css'
 
@@ -9,28 +10,35 @@ import './_FavoriteCard.css'
 function FavoriteCard({info}) {
   const capitalize = (word) => word[0].toUpperCase() + word.slice(1).toLowerCase()
   const updateUser = useUpdateUser()
+  const [favoriteInfo, setFavoriteInfo] = useState(info)
   
   // TODO: Refactor in to reusable hook so this doesnt exist both here and in recipe card component
   const handleClick = () => {
     console.log('removed!')
-    const params = new URLSearchParams({api_key: 'guest_api_key', favorite_id: info.id})
+    const params = new URLSearchParams({api_key: 'guest_api_key', favorite_id: favoriteInfo.id})
 
     fetch(process.env.REACT_APP_BASE_URL + '/api/v1/favorites?' + params, { method: 'DELETE'})
       .then(response => updateUser())
   }
 
+  const updateImageUrl = () => {
+    const { image_url: _, ...newInfo } = favoriteInfo
+    setFavoriteInfo(
+      newInfo
+    )
+  }
+
   return (
     <div className='favorite-container'>
-      {/* TODO: image_urls expire after a certain amount of time */}
-      <img src={info.image_url} className='favorite-image' />
+      <img src={favoriteInfo.image_url ? favoriteInfo.image_url : logo} className='favorite-image' onError={updateImageUrl} />
       <div className='favorite-info'>
-        <h2>{info.recipe_title}</h2>
-        <a href={info.recipe_link}>
+        <h2>{favoriteInfo.recipe_title}</h2>
+        <a href={favoriteInfo.recipe_link}>
           See Recipe
         </a>
         <br/>
-        <Link to={`/country/${info.country}`}>
-          Learn About {capitalize(info.country)}
+        <Link to={`/country/${favoriteInfo.country}`}>
+          Learn About {capitalize(favoriteInfo.country)}
         </Link>
         <br/>
       </div>
